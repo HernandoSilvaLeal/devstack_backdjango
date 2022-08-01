2adc8aeeaa91ac67f0b35785704298f3115c643a
Hernando Silva Leal <hernandosilvaleal@gmail.com>
2022-08-01 15:22:34 -0500

    cambios subidos a la rama main

--- ecomerce_rest/apps/users/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.users'                                     # Es importante usar aqui el nombre con la sub ruta si no arroja error
-                                                            # https://stackoverflow.com/questions/67056517/django-3-2-exception-django-core-exceptions-improperlyconfigured
