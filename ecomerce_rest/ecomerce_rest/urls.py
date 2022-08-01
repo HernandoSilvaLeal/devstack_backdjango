4ba1536153743f43dd1ca27e144c0bbaebafc1a0
Hernando Silva Leal <hernandosilvaleal@gmail.com>
2022-08-01 02:47:36 -0500

    serializador, api_view, modelo, y urls funcionando hasta final de clase 3

--- ecomerce_rest/ecomerce_rest/urls.py
+++ ecomerce_rest/ecomerce_rest/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuario/', include('apps.users.api_views.urls')), 
+    path('usuario/', include('apps.users.api.urls')), 
 ]
