6f747a3247dc4ef4669fefcf09aebf0a865e597b
Hernando Silva Leal <hernandosilvaleal@gmail.com>
2022-07-31 23:29:54 -0500

    Reinicio de todo el proyecto

--- profiles_api/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class HelloSerializer(serializers.Serializer):
-    namep = serializers.CharField(max_length=50) # Serializacion del campo para probar nuestro api view
-    # name2 = serializers.CharField(max_length=50) # Por defecto los campos son obligatorios
-    
