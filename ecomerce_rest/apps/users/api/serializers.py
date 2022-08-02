0ba593d377b1eae3f0a6198cb05b9a998e2634b6
Hernando Silva Leal <hernandosilvaleal@gmail.com>
2022-08-01 22:12:54 -0500

    Creacion de dos nuevas aplicaciones e inicializacion de Modelos

--- ecomerce_rest/apps/users/api/serializers.py
+++ ecomerce_rest/apps/users/api/serializers.py
@@ -8,37 +8,20 @@ class UserSerializer(serializers.ModelSerializer):   # Por convencion se le colo
         # fields = '__all__'                         # Se utiliza fields o exclude, para señalar los campos, para campos especificos usar []
         fields = ['id','password','username','email','name','last_name']
 
+    def create(self,validated_data):                 # Redefinicion del metodo create que sobreescribe el predeterminado del serializer para encriptar contraseña.
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
 
-class TestUserSerializer(serializers.Serializer):     # Este es un ejemplo de serializer que no esta basado en un modelo.
-    name = serializers.CharField()      # Realizo las validaciones customizadas por campo.
-    email = serializers.EmailField()                  # Valida primero el nombre, luego el tipo de dato y luego los atributos
-
-    def validate_name(self,value):                    # Primera validacion por secuencia y nombre de campo
-        # Custom Validation
-        if 'develop' in value:
-            raise serializers.ValidationError('Error, no puede existir un usuario con ese nombre')
-        # print(self.context)
-        return value
-
-    def validate_email(self,value):                   # Segunda validacion por secuencia y nombre de campo
-        # Custom Validation
-        if value == '':
-            raise serializers.ValidationError('Error, Tiene que indicar un correo')
-        
-        # print(self.context)
-        # print(value)
-        if self.context['name'] in self.context.values():             # Antes no aplicaba la excepcion porque faltaba validar los valores de las llaves del dict CONTENIAN a
-            raise serializers.ValidationError('Error, El correo no puede contener el nombre!!!!!!!!!!!!!!')   # Esto es una validacion de campos dependendientes
-
-        return value
-
-    def validate(self,data):                          # Tercera validacion general, y predeterminada que aplica el framework a todos los campos.
-        print('Validate General')
-        return data                                   # Se puede validar por ejemplo que el nombre no se incluya en el correo o la contraseña.
-
-
-    # def create(self,validated_data):
-    #     user = User(**validated_data)
-    #     user.set_password(validated_data['password'])
-    #     user.save()
-    #     return user
+    def to_representation(self, instance):
+        return {
+            'id': instance['id'],
+            'name': instance['name'],
+            'username': instance['username'],
+            'email': instance['email'],
+            'password': instance['password']
+        }
