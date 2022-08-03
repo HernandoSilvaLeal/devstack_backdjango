b76b2d4393011d52962ead5413c530f7f6183f00
Hernando Silva Leal <hernandosilvaleal@gmail.com>
2022-08-02 19:37:09 -0500

    Limpieza de avance de proyecto por restructuracion nnd

--- ecomerce_rest/apps/users/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-# from apps.users.api.api_views import UserAPIView
-from apps.users.api.api_views import user_api_view, user_detail_api_view
-
-urlpatterns = [
-    # path('usuario/', UserAPIView.as_view(), name = 'usuario_api') # El path es = (Endpoint, Funcion que llama la vista, nombre del path) FORMA APIVIEW-CLASES Y FUNCIONES POR METODO
-    path('usuario/', user_api_view, name = 'usuario_api'), # FORMA APIVIEW-DECORADORES Y CONDICIONALES POR METODO, importo la propia funcion, sin .as_view
-    path('usuario/<int:pk>/', user_detail_api_view  , name = 'user_detail_api_view') 
-]
